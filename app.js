da727da14962f3d6cc1340763befcc2f9d39e2b2
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-13 17:40:37 -0700

    Implement display of NewPoll component

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // import utilities
 
 // import component creators
+import createNewPoll from './components/NewPoll.js';
 
 // import state and dispatch functions
 import state from './state.js';
 
 // Create each component:
 // - pass in the root element via querySelector
-// - pass any needed handler functions as properties of an actions object 
+// - pass any needed handler functions as properties of an actions object
+const NewPoll = createNewPoll(document.querySelector('#new-poll'));
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
-    // Call each component passing in props that are the pieces of state this component needs
+    NewPoll({ currentPoll: state.currentPoll });
 }
 
 // Call display on page load
