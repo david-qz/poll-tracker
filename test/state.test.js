7b25e6a890e1b9ba6d629dee3731b00a4195496a
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-13 22:06:38 -0700

    Test and implement voting state actions

--- test/state.test.js
+++ test/state.test.js
@@ -1,6 +1,8 @@
 import state, {
     initialize,
     newPoll,
+    vote,
+    unvote,
 } from '../state.js';
 
 // make sure state is at known starting point
@@ -23,6 +25,49 @@ test('newPoll()', (expect) => {
         option1: {
             response: 'yes',
             votes: 0,
+        },
+        option2: {
+            response: 'no',
+            votes: 0,
+        },
+    });
+});
+
+test('vote()', expect => {
+    newPoll('pizza', 'yes', 'no');
+
+    vote(1);
+    vote(1);
+    vote(2);
+
+    expect.deepEqual(state.currentPoll, {
+        question: 'pizza',
+        option1: {
+            response: 'yes',
+            votes: 2,
+        },
+        option2: {
+            response: 'no',
+            votes: 1,
+        },
+    });
+});
+
+test('unvote()', expect => {
+    newPoll('pizza', 'yes', 'no');
+
+    vote(1);
+    vote(1);
+    vote(2);
+
+    unvote(1);
+    unvote(2);
+
+    expect.deepEqual(state.currentPoll, {
+        question: 'pizza',
+        option1: {
+            response: 'yes',
+            votes: 1,
         },
         option2: {
             response: 'no',
